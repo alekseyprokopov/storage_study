99657c18b2505b5e2e93cd2ba59c6b07c08070c4
Prokopov Aleksey <prokopov.vrn@gmail.com>
2023-03-27 20:00:32 +0300

    add simlpe upload method

--- internal/file/file.go
+++ internal/file/file.go
@@ -1,7 +1,22 @@
 package file
 
+import "github.com/google/uuid"
+
 type File struct {
-	ID
+	ID   uuid.UUID
 	Name string
 	Data []byte
+}
+
+func New(filename string, blob []byte) (*File, error) {
+	uid, err := uuid.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+
+	return &File{
+		Name: filename,
+		Data: blob,
+		ID:   uid,
+	}, nil
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,9 +1,14 @@
 package storage
 
-type Storage struct {
+import "github.com/alekseyprokopov/storage/internal/file"
 
+type Storage struct {
 }
 
 func New() *Storage {
 	return &Storage{}
+}
+
+func (s *Storage) Upload(filename string, blob []byte) (*file.File, error) {
+	return file.New(filename, blob)
 }
